9e1d1bbaa3dafc11340f2fb3ce459063452d96d1
AlessandroMessineo <alessandromessineo46@gmail.com>
2025-09-08 16:42:56 +0200

    generazione numeri casuali

--- js/script.js
+++ js/script.js
@@ -9,3 +9,33 @@
 // BONUS:
 // Inseriamo la validazione: se l'utente mette due numeri uguali o inserisce cose diverse da numeri lo blocchiamo in qualche modo.
 // Se l’utente ha inserito qualcosa di non valido, segnaliamolo visivamente nel form.
+
+
+const countdownElement = document.getElementById("countdown");
+const numbersListElement = document.getElementById('numbers-list');
+const formElement = document.getElementById('answers-form');
+const inputUtente = document.querySelectorAll("input");
+const messageElement = document.getElementById('message');
+
+
+const min = 1;
+const max = 50;
+
+function randomNum(min, max) {
+
+    let arrayRandomNum = [];
+
+    for (let i = 0; arrayRandomNum.length < 5; i++) {
+
+        let num = Math.floor(Math.random() * (max - min + 1)) + min;
+        if (!arrayRandomNum.includes(num)) {
+            arrayRandomNum.push(num);
+        }
+        numbersListElement.innerHTML = `<li>${arrayRandomNum[0]}</li><li>${arrayRandomNum[1]}</li><li>${arrayRandomNum[2]}</li><li>${arrayRandomNum[3]}</li><li>${arrayRandomNum[4]}</li>`;
+    }
+    return arrayRandomNum;
+}
+
+
+
+console.log(randomNum(min, max));
